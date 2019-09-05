7ab4b783386f93cd629cf94a8541c6b1d8572ebf
Natan Oliveira <natannjos@gmail.com>
2019-09-05 07:59:15 -0300

    Remove env.js from repo

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
+var base = "192.168.1.128:8000";
+
 export const base_urls = {
-  API_URL: process.env.API_URL || "http://192.168.100.5:8000/api",
-  API_URL_BROWSER:
-    process.env.API_URL_BROWSER || "http://192.168.100.5:8000/api",
-  WS_URL: process.env.WS_URL || "192.168.100.5:8000"
+  API_URL: process.env.API_URL || `http://${base}/api`,
+  API_URL_BROWSER: process.env.API_URL_BROWSER || `http://${base}/api`,
+  WS_URL: process.env.WS_URL || `${base}`
 };
