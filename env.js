bf16dd474efeb8f0be1a4c6783fed385ea3f198e
Natan Oliveira <natannjos@gmail.com>
2019-09-06 13:27:55 -0300

    Remove env from git

--- env.js
+++
@@ -1,7 +0,0 @@
-var base = "192.168.1.128:8000";
-
-export const base_urls = {
-  API_URL: process.env.API_URL || `http://${base}/api`,
-  API_URL_BROWSER: process.env.API_URL_BROWSER || `http://${base}/api`,
-  WS_URL: process.env.WS_URL || `${base}`
-};
